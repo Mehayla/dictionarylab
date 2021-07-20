ad36e4784fa521929ee55edf58d270b5bcbde74f
Mehayla <mehaylac@gmail.com>
2021-07-20 01:19:23 +0000

    Making it look pretty

--- ratings.py
+++ ratings.py
@@ -1,19 +1,19 @@
 """Restaurant rating lister."""
 
-# opening the scores.txt file and attaching to variable
-score_file = open('scores.txt')
-# reads through the whole file
-score_file = score_file.read()
+
+score_file = open('scores.txt') # opening the scores.txt file and attaching to variable
+score_file = score_file.read() # reads through the whole file
 res_dictionary = {}
 
 def res_ratings(file):
-    line = score_file.split('\n')
+    line = score_file.split('\n') #Can also use .strip(/r or /n) instead
 
     for row in line:
         split_lines = row.split(':')
         key = split_lines[0]
         value = split_lines[1]
         res_dictionary[key] = value
+        
     keys = res_dictionary.keys()
     sort_keys = sorted(keys)
 
